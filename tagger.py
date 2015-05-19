be9621e735d5d2724b2be7803cbf6f437028892d
Terrance Cohen <terrance.cohen@gmail.com>
2015-05-18 22:56:59 -0700

    pygame test with first tagged image

--- tagger.py
+++ tagger.py
@@ -2,6 +2,9 @@ import os
 import os.path
 import ConfigParser
 
+import sys
+import pygame
+
 def run():
 
     config = ConfigParser.ConfigParser()
@@ -19,6 +22,20 @@ def run():
         print "Error: source_tag_filename " + source_tag_filename + " not found. Exiting."
         return False
 
+    root_dir = os.path.expanduser(config.get(general_section, "RootDir"))
+    if not os.path.isdir(root_dir):
+        print "Error: root_dir " + root_dir + " not found. Exiting."
+        return False
+    else:
+        print "Ok: using root_dir " + root_dir
+
+    #pygame
+    pygame.init()
+    size = width, height = 800, 600
+    speed = [4, 4]
+    white = 255, 255, 255
+    screen = pygame.display.set_mode(size)
+
     source_tag_file = open(source_tag_filename, "r")
 
     for line in source_tag_file:
@@ -28,9 +45,32 @@ def run():
         if len(line) == 0 or line[0] == "#":
             continue
 
-        print "  " + line
+        photo_filename = os.path.join(root_dir, line)
+        print "  " + photo_filename
+
+        ball = pygame.image.load(photo_filename)
+        ball = pygame.transform.scale(ball, (100, 100))
+        ballrect = ball.get_rect()
+
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    break
+
+            ballrect = ballrect.move(speed)
+            if ballrect.left < 0 or ballrect.right > width:
+                speed[0] = -speed[0]
+            if ballrect.top < 0 or ballrect.bottom > height:
+                speed[1] = -speed[1]
+
+            screen.fill(white)
+            screen.blit(ball, ballrect)
+            pygame.display.flip()
+
+        break
 
     source_tag_file.close()
+    sdl2.ext.quit()
 
     return True
 
